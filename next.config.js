53bc31e7b57ce1e73ec1345fb12b978b8b1dac70
Patrick Schmidt <p.schmidt@goconut.com>
2025-12-01 00:02:16 +0100

    added possible fix for missing module zlib

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    BOT_TOKEN: process.env.BOT_TOKEN,
-    PUBLIC_KEY: process.env.PUBLIC_KEY,
-    REGISTER_COMMANDS_KEY: process.env.REGISTER_COMMANDS_KEY,
-  },
+    reactStrictMode: true,
+    env: {
+        BOT_TOKEN: process.env.BOT_TOKEN,
+        PUBLIC_KEY: process.env.PUBLIC_KEY,
+        REGISTER_COMMANDS_KEY: process.env.REGISTER_COMMANDS_KEY,
+    },
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.node/,
+            use: 'node-loader'
+        })
+
+        return config
+    },
 }
 
 module.exports = nextConfig
