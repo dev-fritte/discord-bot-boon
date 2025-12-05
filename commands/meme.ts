0a78bab8e520de4dbd49b650a13e838771d8f2c4
Patrick Schmidt <p.schmidt@goconut.com>
2025-12-05 18:27:44 +0100

    changed interaction response

--- commands/meme.ts
+++ commands/meme.ts
@@ -1,6 +1,6 @@
 import {SlashCommandBuilder} from '@discordjs/builders';
 import {executeCommand} from '@/types';
-import {AttachmentBuilder} from 'discord.js'
+import {AttachmentBuilder, ChatInputCommandInteraction} from 'discord.js'
 import {createCanvas, loadImage} from '@napi-rs/canvas'
 
 type MemeResponse = {
@@ -15,37 +15,33 @@ export const register = new SlashCommandBuilder()
     .setName('meme')
     .setDescription('responds with a random meme with matching tags');
 
-export const execute: executeCommand = async (interaction) => {
+export const execute: executeCommand = async (interaction: ChatInputCommandInteraction) => {
+    
     // You have access to do interaction object
     // https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
 
     // const tags: string[] = interaction.
     // const tags: string[] = interaction.options.getString('tags');
 
-    const tagsAppendix =  'tags=kai'//'tags=' + tags.map(tag => '&tags=' + tag);
+    const tagsAppendix = 'tags=kai'//'tags=' + tags.map(tag => '&tags=' + tag);
 
     console.log('tagAppendix', tagsAppendix);
 
-    const response = await fetch(`https://next-picture-storage.vercel.app/memes/find?${tagsAppendix}`)
+    const memeResponse = await fetch(`https://next-picture-storage.vercel.app/memes/find?${tagsAppendix}`)
         .then(res => res.json() as unknown as MemeResponse);
 
-    console.log('picture storage response', response)
+    console.log('picture storage response', memeResponse)
 
     const canvas = createCanvas(200, 200)
     const context = canvas.getContext('2d');
-    const background = await loadImage(response.blob_url);
+    const background = await loadImage(memeResponse.blob_url);
     // This uses the canvas dimensions to stretch the image onto the entire canvas
     context.drawImage(background, 0, 0, canvas.width, canvas.height);
     // Use the helpful Attachment class structure to process the file for you
-    const attachment = new AttachmentBuilder(await canvas.encode('png'), {name: 'random-meme.png'});
+    const meme = new AttachmentBuilder(await canvas.encode('png'), {name: 'random-meme.png'});
 
-    // you should return a APIInteractionResponse
-    // https://discord-api-types.dev/api/discord-api-types-v10#APIApplicationCommandInteraction
-    return {
-        type: 4,
-        data: {
-            attachment: attachment,
-            content: `Hier ist dein Meme ${interaction.member?.user.username}`,
-        },
-    };
+    await interaction.reply({
+        content: `Hier ist dein Meme ${interaction.member?.user.username}`,
+        files: [meme],
+    })
 };
